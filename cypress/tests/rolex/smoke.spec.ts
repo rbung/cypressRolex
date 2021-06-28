c276d055e29649c6dad03fc3e53341de467991d1
Rodolphe BUNG <rkbung@gmail.com>
2021-06-28 22:31:18 +0200

    fix: make smoke test works on mobile

--- cypress/tests/rolex/smoke.spec.ts
+++ cypress/tests/rolex/smoke.spec.ts
@@ -1,3 +1,5 @@
+import { isMobile } from "../../support/utils";
+
 describe("smoke tests", () => {
   it("should pass all the steps", () => {
     cy.visit("/");
@@ -6,24 +8,36 @@ describe("smoke tests", () => {
     cy.get("#password").type("s3cret{enter}");
 
     cy.location("pathname").should("eq", "/");
+    if (isMobile()) {
+      cy.get("[data-test=sidenav-toggle").click();
+    }
     cy.get("[data-test=sidenav-home] ").should("be.visible");
     cy.get("[data-test=sidenav-user-settings] ").should("be.visible");
     cy.get("[data-test=sidenav-bankaccounts] ").should("be.visible");
     cy.get("[data-test=sidenav-notifications]").should("be.visible");
     cy.get("[data-test*=transaction-sender]").should("have.length.gt", 0);
 
+    if (isMobile()) {
+      cy.get("[data-test=sidenav-home] ").click();
+    }
     cy.get("[data-test=nav-contacts-tab]").click();
     cy.get("[data-test*=transaction-sender]").should("have.length.gt", 0);
 
     cy.get("[data-test=nav-personal-tab]").click();
     cy.get("[data-test*=transaction-sender]").should("have.length.gt", 0);
 
+    if (isMobile()) {
+      cy.get("[data-test=sidenav-toggle").click();
+    }
     cy.get("[data-test=sidenav-user-settings] ").click();
     cy.get("[data-test=user-settings-firstName-input]").should("have.value", "Edgar");
     cy.get("[data-test=user-settings-lastName-input]").should("have.value", "Johns");
     cy.get("[data-test=user-settings-email-input]").should("have.value", "Norene39@yahoo.com");
     cy.get("[data-test=user-settings-phoneNumber-input]").should("have.value", "625-316-9882");
 
+    if (isMobile()) {
+      cy.get("[data-test=sidenav-toggle").click();
+    }
     cy.get("[data-test=sidenav-signout]").click();
     cy.location("pathname").should("eq", "/signin");
   });
